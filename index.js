89bac16a9656824114244774b889b81c1c1bca5c
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-09-03 21:07:29 +0600

    server changed

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const cors=require('cors')
+const cors = require('cors')
 const bodyParser = require('body-parser')
-const { MongoClient , ObjectId } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 require('dotenv').config();
 
 
@@ -26,69 +26,69 @@ client.connect(err => {
   const orderCollection = client.db("autoParts").collection("orders");
 
   //---upload product--//
-app.post('/addProduct', (req, res)=>{
-    const products= req.body;
+  app.post('/addProduct', (req, res) => {
+    const products = req.body;
     console.log(products);
     productsCollection.insertOne(products)
-    .then(result =>{
+      .then(result => {
         res.send(result.insertedCount > 0)
-    })
-})
-
-//--get all products--//
-app.get('/products' , (req, res)=>{
-  productsCollection.find({})
-  .toArray((err, documents)=>{
-      res.send(documents)
-  })
-})
-
-//----find single product by id ---//
-app.get('/product/:id',(req,res) => {
-  const id = ObjectId(req.params.id)
-  // console.log('id',id);
-  productsCollection.find({_id: id})
-  .toArray((err, documents) => {
-  
-      res.send(documents[0])
-  })
-})
-
-//----post for order --- //
-app.post('/addOrder' , (req,res)=>{
-  const orderDetails = req.body;
-  // console.log(orderDetails);
-  orderCollection.insertOne(orderDetails)
-  .then( result => {
-    res.send(result.insertedCount  > 0)
+      })
   })
 
-})
-
-//---show orders products---//
-app.get('/showOrderProduct/:email', (req,res) => {
-  const email = req.params.email;
-  console.log('email',email);
-  orderCollection.find({email: email})
-  .toArray((err,documents) => {
-      res.send(documents)
+  //--get all products--//
+  app.get('/products', (req, res) => {
+    productsCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
   })
-})
+
+  //----find single product by id ---//
+  app.get('/product/:id', (req, res) => {
+    const id = ObjectId(req.params.id)
+    // console.log('id',id);
+    productsCollection.find({ _id: id })
+      .toArray((err, documents) => {
+
+        res.send(documents[0])
+      })
+  })
+
+  //----post for order --- //
+  app.post('/addOrder', (req, res) => {
+    const orderDetails = req.body;
+    // console.log(orderDetails);
+    orderCollection.insertOne(orderDetails)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+
+  })
+
+  //---show orders products---//
+  app.get('/showOrderProduct/:email', (req, res) => {
+    const email = req.params.email;
+    console.log('email', email);
+    orderCollection.find({ email: email })
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
+  })
 
 
-// delete product
-app.delete('/deleteProduct/:id',(req,res) => {
-  const id = ObjectId(req.params.id)
-  productsCollection?.deleteOne({
+  // delete product
+  app.delete('/deleteProduct/:id', (req, res) => {
+    const id = ObjectId(req.params.id)
+    productsCollection?.deleteOne({
       _id: id
+    })
+      .then(result => {
+        console.log('delete', result)
+        res.send(result.deletedCount > 0)
+      })
   })
-  .then( result => {
-      console.log('delete',result)
-      res.send(result.deletedCount > 0)
-  })
-})
   console.log('mongodb working');
-//   client.close();
+  //   client.close();
 });
 
 
